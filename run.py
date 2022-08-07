a6e6b64fe979af5015015295c9ec0fd287baf943
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-08-07 09:50:13 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-import os
+import platform,os
+
 os.system('git pull')
-if __name__ == "__main__":
-   try:
-       __import__("cy").lisensi()
-   except Exception as e:
-       exit(str(e))
+
+os.system('clear')
+
+os.system('python run.py')
